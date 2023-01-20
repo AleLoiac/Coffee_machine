2adb472e18059c8201e4805eca6cfd3ba2c719b2
AleLoiac <alessandroloiac@gmail.com>
2023-01-20 11:48:27 +0100

    20/01

--- Coffee_machine.go
+++ Coffee_machine.go
@@ -10,4 +10,17 @@ func main() {
 	fmt.Println("Pouring coffee into the cup")
 	fmt.Println("Pouring some milk into the cup")
 	fmt.Println("Coffee is ready!")
+
+	var numCoffee int
+	const water int = 200
+	const milk int = 50
+	const beans int = 15
+
+	fmt.Println("Write how many cups of coffee you will need:")
+
+	fmt.Scan(&numCoffee)
+	fmt.Println("For", numCoffee, "cups of coffee you will need:")
+	fmt.Println(water*numCoffee, "ml of water")
+	fmt.Println(milk*numCoffee, "ml of milk")
+	fmt.Println(beans*numCoffee, "g of coffee beans")
 }
